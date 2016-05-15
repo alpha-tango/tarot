208865c22a87d6b6ae532b931266431b4eef1080
alpha-tango <atpope5@gmail.com>
2016-05-15 17:59:13 -0400

    + Deck and Card models

--- tarot/settings.py
+++ tarot/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'tarot_app.apps.TarotAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- tarot_app/models.py
+++ tarot_app/models.py
@@ -2,4 +2,18 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Deck(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=500)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.name
+
+class Card(models.Model):
+    deck = models.ForeignKey(Deck, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    image_url = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
