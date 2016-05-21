f0024028b84ec51d99e1b8e94a9a8ade008ee71f
alpha-tango <atpope5@gmail.com>
2016-05-20 21:32:56 -0400

    + deck detail view

--- tarot_app/views.py
+++ tarot_app/views.py
@@ -1,5 +1,13 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from .models import Deck
 
 def index(request):
-	return HttpResponse("Hello, world. You're at the polls index.")
+	latest_deck_list = Deck.objects.order_by('-pub_date')[:10]
+	context = {
+		'latest_deck_list': latest_deck_list,
+	}
+	return render(request, 'tarot_app/index.html', context)
+
+def detail(request, deck_id):
+	deck = get_object_or_404(Deck, pk=deck_id)
+	return render(request, 'tarot_app/detail.html', {'deck': deck})
