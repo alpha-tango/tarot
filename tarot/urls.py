e59cb365fe08d1e728a6e3647501c8c6dec835cb
alpha-tango <atpope5@gmail.com>
2016-10-06 22:17:58 -0400

    + first pass at logins

--- tarot/urls.py
+++ tarot/urls.py
@@ -1,9 +1,14 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^decks/', include('decks.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^login/$', auth_views.login, name='login'),
+    url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
+    url(r'^', include('registration.backends.simple.urls'))
+
 ]
