6f0ffdacf837a8338f71790f69e098e2ad0685ef
alpha-tango <atpope5@gmail.com>
2017-10-04 21:08:02 -0500

    + list suits

--- learn/urls.py
+++ learn/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from learn import views
 
 urlpatterns = [
+    url(r'^suits$', views.SuitList.as_view(), name='suit-list'),
     url(r'^symbolism$', views.symbolism, name='symbolism'),
     url(r'^tips$', views.tips, name='tips'),
     url(r'^layouts$', views.layouts, name='layouts'),
--- learn/views.py
+++ learn/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from learn.models import Suit
 
 # Create your views here.
 
 def index(request):
     return render(request, 'learn/index.html')
+
+class SuitList(ListView):
+    model = Suit
 
 def symbolism(request):
     return render(request, 'learn/symbolism.html')
