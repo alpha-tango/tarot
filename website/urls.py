3ac42300655fd71aea5cff60826c2f52372c8342
alpha-tango <atpope5@gmail.com>
2017-10-10 18:15:32 -0500

    + redirect homepage to /learn

--- website/urls.py
+++ website/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from website import views
 
 urlpatterns = [
+    url(r'^$', views.index, name='index'),
     url('^tarot/', include('tarot.urls', namespace="tarot")),
     url('^learn/', include('learn.urls', namespace="learn")),
     url(r'^admin/', admin.site.urls),
